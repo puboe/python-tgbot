495bb0341a2d1a04933dcd39a3db0baf1c328239
Pablo Diaz <pablo_92@live.com.ar>
2015-05-12 19:57:36 -0300

    idem

--- commands/motion_cmd.py
+++ commands/motion_cmd.py
@@ -7,12 +7,10 @@ class MotionCmd(command.BotCommand):
         print (contents)
         if len(contents) < 2 or len(contents) >= 3:
 			return msg.BotMsg('Usage: .motion [start | stop]')
-
-        output = self.process_output('motion ' + cmd)
-
-        resp = msg.BotMsg('Motion: ' + cmd)
-
-        return resp
+        else:
+            output = self.process_output('motion ' + cmd)
+            resp = msg.BotMsg('Motion: ' + cmd)
+            return resp
 
     def process_output(self, command):
         return subprocess.check_output(command.split()).decode().strip()
