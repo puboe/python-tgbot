472767ec3977386e32e06c90450fb2cc4451acdf
Pablo Diaz <pablo_92@live.com.ar>
2015-05-12 19:59:45 -0300

    fixes

--- commands/motion_cmd.py
+++ commands/motion_cmd.py
@@ -5,14 +5,14 @@ class MotionCmd(command.BotCommand):
     def run(self, dest, contents):
         print ('Motion contents:')
         print (contents)
-        if len(contents) < 2 or len(contents) >= 3:
-			return msg.BotMsg('Usage: .motion')
-        else:
-            output = self.process_output('motion ' + cmd)
-            resp = msg.BotMsg('Motion: ' + cmd)
-            return resp
+        if len(contents) < 2:
+			return msg.BotMsg('Usage: .motion [start | stop]')
+
+        output = self.process_output('motion ' + cmd)
+        resp = msg.BotMsg('Motion: ' + cmd)
+        return resp
 
     def process_output(self, command):
         return subprocess.check_output(command.split()).decode().strip()
 
-command_instance = StatusCmd(bindings = ['motion'], name = 'motion')
+command_instance = MotionCmd(bindings = ['motion'], name = 'motion')
