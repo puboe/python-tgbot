5e49bd824cd9b8f486d40c34ae81b6465804c838
Pablo Diaz <pablo_92@live.com.ar>
2015-05-12 20:49:14 -0300

    indentation fix

--- commands/motion_cmd.py
+++ commands/motion_cmd.py
@@ -6,8 +6,10 @@ class MotionCmd(command.BotCommand):
         print ('Motion contents:')
         print (contents)
         if len(contents) < 2:
-			return msg.BotMsg('Usage: .motion [start | stop]')
+            return msg.BotMsg('Usage: .motion [start | stop]')
 
+
+        cmd = contents[1]
         output = self.process_output('motion ' + cmd)
         resp = msg.BotMsg('Motion: ' + cmd)
         return resp
