6dd3eb78ceac8aa15a39eb6936e2db2fe621a15d
Pablo Diaz <pablo_92@live.com.ar>
2015-05-12 19:54:21 -0300

    fix

--- commands/motion_cmd.py
+++ commands/motion_cmd.py
@@ -3,8 +3,8 @@ from bot import command, msg
 
 class MotionCmd(command.BotCommand):
     def run(self, dest, contents):
-        print "Motion contents:"
-        print contents
+        print ('Motion contents:')
+        print (contents)
         if len(contents) < 2 or len(contents) >= 3:
 			return msg.BotMsg('Usage: .motion [start | stop]')
 
