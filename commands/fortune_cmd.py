ef6361cd8bc8de149cf084df5fadd98cbb612cf6
Pablo Diaz <pablo_92@live.com.ar>
2015-05-13 01:00:08 -0300

    fortune fix

--- commands/fortune_cmd.py
+++ commands/fortune_cmd.py
@@ -1,14 +1,21 @@
 import subprocess
 from bot import command, msg
 
-class MotionCmd(command.BotCommand):
+class FortuneCmd(command.BotCommand):
     def run(self, dest, contents, passphrase):
 
-        output = self.process_output('fortune')
+
+        output = self.process_output('fortune -n 110 -s')#.split('\n')
+        print(output)
+
         resp = msg.BotMsg(output)
+        #for x in list(range(1,len(output))):
+        #    print (x)
+        #    resp.add_line(output[x])
+
         return resp
 
     def process_output(self, command):
         return subprocess.check_output(command.split()).decode().strip()
 
-command_instance = MotionCmd(bindings = ['f', "fortune"], name = 'fortune')
+command_instance = FortuneCmd(bindings = ['f', "fortune"], name = 'fortune')
