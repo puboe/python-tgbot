27a5449ea491bb3086eb9d9b75a3684528148b01
Pablo Diaz <pablo.diazuboe@mercadolibre.com>
2015-05-13 00:09:48 -0300

    Fortune command

---
+++ commands/fortune_cmd.py
@@ -0,0 +1,14 @@
+import subprocess
+from bot import command, msg
+
+class MotionCmd(command.BotCommand):
+    def run(self, dest, contents, passphrase):
+
+        output = self.process_output('fortune')
+        resp = msg.BotMsg(output)
+        return resp
+
+    def process_output(self, command):
+        return subprocess.check_output(command.split()).decode().strip()
+
+command_instance = MotionCmd(bindings = ['motion'], name = 'motion')
